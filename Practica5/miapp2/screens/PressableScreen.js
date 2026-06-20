ff3e3365229cf6e13dcd69d63984a73c21938a2f
AnitaACD <anaf51192@gmail.com>
2026-06-19 20:25:22 -0600

    Pressable & Switch

--- Practica5/miapp2/screens/PressableScreen.js
+++ Practica5/miapp2/screens/PressableScreen.js
@@ -1,31 +1,89 @@
-/*Zona 1 : importaciones de componentes y archivos */
+import { useState } from 'react';
+import {StyleSheet, Text, View, Switch, Pressable, Alert} from 'react-native';
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-
-
-/*Zona 2: main  */
 export default function PressableScreen() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleSwitch = () => setIsDarkMode(previousState => !previousState);
+
   return (
-    <View style={styles.container}>
-      <Text> Aqui va la practica de Roque y Armando </Text>
+    <View style={[styles.container, isDarkMode ? styles.bgDark : styles.bgLight]}>
 
-      <StatusBar style="auto" />
+      <Text style={[styles.title, isDarkMode ? styles.textDark : styles.textLight]}>
+        Ajustes del Sistema
+      </Text>
 
+      <View style={styles.row}>
+
+        <Text
+          style={[styles.label, isDarkMode ? styles.textDark : styles.textLight]}>
+          Modo Oscuro {isDarkMode ? 'Activado' : 'Desactivado'}
+        </Text>
+
+        <Switch
+          trackColor={{ false: 'black', true: 'white' }}
+          thumbColor={isDarkMode ? 'yellow' : 'gray'}
+          onValueChange={toggleSwitch}
+          value={isDarkMode}
+        />
+
+      </View>
+
+      <Pressable
+        onPress={() => Alert.alert('Éxito', 'Has presionado el botón')}
+        onLongPress={() => Alert.alert('Información', 'Se mantuvo presionado')}
+        delayLongPress={1000}
+        style={({ pressed }) => [
+          styles.button,
+          {
+            backgroundColor: pressed ? 'blue' : 'green'
+          }
+        ]}
+      >
+        <Text style={styles.buttonText}>
+          Guardar Cambios
+        </Text>
+      </Pressable>
 
     </View>
   );
 }
 
-/*Zona 3: EStilos y Posicionamiento */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection:'colum'
+    padding: 20,
   },
-
+  bgLight: { backgroundColor: '#f5f5f5' },
+  bgDark: { backgroundColor: '#121212' },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  textLight: { color: '#000' },
+  textDark: { color: '#fff' },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 40,
+    paddingHorizontal: 20,
+  },
+  label: { fontSize: 18 },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    elevation: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
