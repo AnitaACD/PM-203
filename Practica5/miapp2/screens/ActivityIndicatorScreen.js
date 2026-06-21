db4e5456d7b75204c92b5273bcc5135216254d79
AnitaACD <anaf51192@gmail.com>
2026-06-20 22:09:05 -0600

    ActivityIndicator and KeyboardAvoidingView

--- Practica5/miapp2/screens/ActivityIndicatorScreen.js
+++ Practica5/miapp2/screens/ActivityIndicatorScreen.js
@@ -1,31 +1,54 @@
-/*Zona 1 : importaciones de componentes y archivos */
-
+/*Zona 1: Importaciones de componentes y archivos */
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  Text,
+  Platform,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
+  ActivityIndicator,
+} from 'react-native';
 
-
-
-/*Zona 2: main  */
+/*Zona 2: Main - Hogar de los componentes */
 export default function ActivityIndicatorScreen() {
+
+  const [cargando, setCargando] = useState(false);
+
+  const handleSubmit = () => {
+    Keyboard.dismiss();
+    setCargando(true);
+    setTimeout(() => {
+      setCargando(false);
+    }, 2000);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text> Aqui va la practica de Ana </Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
+      enabled ={true}
+      style={{ flex: 1 }}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{ padding: 24, flex: 1, justifyContent: 'space-around' }}>
 
+          <Text style={{ fontSize: 36, marginBottom: 48 }}>Header</Text>
+
+          <TextInput
+            placeholder="Username" style={{ height: 40, borderColor: '#000000', borderBottomWidth: 1, marginBottom: 36 }}/>
+
+          {cargando && (<ActivityIndicator animating={true} size="large" color="green" style={{ marginVertical: 12 }} />)}
+
+          <View style={{ backgroundColor: 'white', marginTop: 12 }}>
+            <Button title="Submit" onPress={handleSubmit} />
+          </View>
+
+        </View>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
-
-
-    </View>
+    </KeyboardAvoidingView>
   );
 }
-
-/*Zona 3: EStilos y Posicionamiento */
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection:'colum'
-  },
-
-});
